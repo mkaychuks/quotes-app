83f8f390fe91ca6c1f39a969035cba684c3cb2cd
mkaychuks <sachufusi@gmail.com>
2024-05-21 15:39:41 +0100

    Setting up MaterialTheme and installing some dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,9 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // google font
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.7")
 }
 
 kapt {
